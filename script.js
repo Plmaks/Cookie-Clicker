d4ef90eb44ccc09a7dd888e3877ca37f4e4f31b8
Plmaks <maks.bor@vp.pl>
2023-12-10 23:38:42 +0100

    Added keybinds, 1 bugfix, made workers better and so on

--- script.js
+++ script.js
@@ -1,32 +1,68 @@
 // Variable Inits
-let cookies, smitog, add, cost1, upg1_amount, slaves, cost2, whips
-// Var Declars
-whips = 0
+let cookies, smitog, add, cost1, upg1_amount, slaves, cost2, Key, Pressed
+// Global Var Declars
 slaves = 0
+Working = false
+upg1_amount = 1
+upg2_amount = 2
+cost1 = 7 + (upg1_amount * 3)
+cost2 = 17 + (upg2_amount * 3)
+
+//Keybinds
+document.addEventListener("keydown", Getkey)
+
+function Getkey(Pressed) {
+    Key = Pressed.key
+    
+    switch (Key) {
+        case "1":
+            upgrades(upg1)
+            break;
+        case "2":
+            upgrades(upg2)
+            break;
+        default:
+            console.log(Key)
+            break;
+    }
+
+    return Key
+}
+//
+
+// Save System, will do later, hopefully
+function Save() {
+
+}
+
+function Load() {
+
+}
 
 function earn() {
-    // Initialization
     cookies = parseInt(document.getElementById("amount").innerHTML)
     smitog = document.getElementById("smily")
+    
+    //If add isn't defined, set it as 1
     if (typeof add === "undefined") {
         add = 1
     } else {}
 
-    if (cookies > 1000 && cookies < 2500) {
+    //Flavor text
+    if (cookies > 1000 && cookies < 5000) {
         smitog.innerHTML = ":)"
-    } else if (cookies > 2500 && cookies < 3000) {
+    } else if (cookies > 5000 && cookies < 10000) {
         smitog.innerHTML = "Thanks for enjoying my game!"
-    } else if (cookies > 3000) {
+    } else if (cookies > 10000) {
         smitog.innerHTML = ""
     } else {
         smitog.innerHTML = ""
     }
-
-    // Math
+    
+    // Updating cookie count
     cookies = cookies + add   
     document.getElementById("amount").innerHTML = cookies
 
-    // le return
     return cookies, smitog, add, amount
 }
 
@@ -35,15 +71,15 @@ function Workerspawn() {
 }
 
 function Worker() {
-    if (whips != 0) {
+    //Global flag, so buying the upgrade doesn't inflate the amount of times that mouse workers are called
+    Working = true
+    
+    //While is too fast
+    if (1 == 1) {
         cookies = parseInt(document.getElementById("amount").innerHTML)
-        whips--
         cookies = cookies + slaves
         document.getElementById("amount").innerHTML = cookies
-        console.log(whips)
         setTimeout(Worker, 1000)
-    } else if (whips != 10 && whips < 9,8,7,6,5,4,3,2,1) {
-        whips = 10
     }
 
     return cookies, slaves, whips
@@ -51,14 +87,14 @@ function Worker() {
 
 function upgrades(type) {
     switch(type) {
+       //Mouse upgrade 
        case upg1:
         upg1_amount = parseInt(document.getElementById("upg1_amount").innerHTML) + 1
-        cost1 = 8 + (upg1_amount * 2)
         if (cookies >= cost1) {
             add++
             cookies = cookies - cost1
-            // Re-setting shit up
-            cost1 = cost1 + (upg1_amount * 2)
+            // Updating values
+            cost1 = cost1 + (upg1_amount * 3)
             document.getElementById("upg1_amount").innerHTML = upg1_amount
             document.getElementById("upg1_cost").innerHTML = cost1
             document.getElementById("amount").innerHTML = cookies
@@ -66,28 +102,34 @@ function upgrades(type) {
             alert("No money?")
         }
         return add, cost1, cookies, upg1_amount
+       //Worker mouse upgrade
        case upg2:
         upg2_amount = parseInt(document.getElementById("upg2_amount").innerHTML) + 1
-        cost2 = 18 + (upg2_amount * 2)
-        if (cookies >= cost2) {
+        if (cookies >= cost2 && Working == false) {
             slaves++
             cookies = cookies - cost2
             document.getElementById("amount").innerHTML = cookies
             // Re-setting part 2: The Eletric Boogalo
-            cost2 = cost2 + (upg2_amount * 2)
+            cost2 = cost2 + (upg2_amount * 3)
             document.getElementById("upg2_amount").innerHTML = upg2_amount
             document.getElementById("upg2_cost").innerHTML = cost2
             document.getElementById("amount").innerHTML = cookies
-            Worker() 
+            Workerspawn()
+            Worker()
+        } else if (cookies >= cost2 && Working == true) {
+            slaves++
+            cookies = cookies - cost2
+            document.getElementById("amount").innerHTML = cookies
+            // Setting up
+            cost2 = cost2 + (upg2_amount * 3)
+            document.getElementById("upg2_amount").innerHTML = upg2_amount
+            document.getElementById("upg2_cost").innerHTML = cost2
+            document.getElementById("amount").innerHTML = cookies
             Workerspawn()
         } else {
             alert("Just admit you are broke, boy")
         }
-        if (slaves != 0) {
-            document.getElementById("Whip").style.visibility = "visible"
-        } else {
-            document.getElementById("Whip").style.visibility = "hidden"
-        }
+        
         return slaves, upg2_amount, cost2, cookies
        default:
         break;
